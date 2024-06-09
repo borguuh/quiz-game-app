87ad34ada32be6318d97a7db6ffb19f7c85cb273
Borguuh <abdrahmanadewale@gmail.com>
2024-06-09 16:20:38 +0100

    create quiz module and setup graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { QuizModule } from './quiz/quiz.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true
+    }),
+    QuizModule,
+  ],
 
 })
 export class AppModule {}
